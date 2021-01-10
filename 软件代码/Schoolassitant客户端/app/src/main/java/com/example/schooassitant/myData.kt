9c1e4e986183ae962c62270132c83d93d97057a9
“zzzxj-zxj” <704893274@qq.com>
2021-01-10 10:43:46 +0800

    log message

--- 软件代码/Schoolassitant客户端/app/src/main/java/com/example/schooassitant/myData.kt
+++
@@ -1,9 +0,0 @@
-package com.example.schooassitant
-
-import android.graphics.Bitmap
-
-object  myData {
-
-    public var bookList = ArrayList<Book>()
-    public var imageList=ArrayList<Bitmap>()
-}
